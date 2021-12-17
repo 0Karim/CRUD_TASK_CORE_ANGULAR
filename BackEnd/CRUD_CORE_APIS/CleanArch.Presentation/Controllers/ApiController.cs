ccc1fc473007c2f074f9294462e98859faef0288
Karim <Karim@DESKTOP-FU75D5J>
2021-12-17 19:25:50 +0200

    tst

--- BackEnd/CRUD_CORE_APIS/CleanArch.Presentation/Controllers/ApiController.cs
+++ BackEnd/CRUD_CORE_APIS/CleanArch.Presentation/Controllers/ApiController.cs
@@ -17,6 +17,8 @@ namespace CleanArch.Presentation.Controllers
     {
         private IMediator _mediator;
 
+
+
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 
